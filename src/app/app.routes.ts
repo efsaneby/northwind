491d0c5d6b915189a8dd5a6687c6bba166874a83
efsaneby <bkocadag@gmal.com>
2023-12-25 15:59:04 +0100

    25-12-2023 commit2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { ProductComponent } from './components/product/product.component';
 
 export const routes: Routes = [
     {path:"", pathMatch: "full", component:ProductComponent},
-    {path:"products", component:ProductComponent}
+    {path:"products", component:ProductComponent},
+    {path:"products/category/:categoryId", component:ProductComponent}
 ];
